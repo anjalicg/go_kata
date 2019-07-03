a2a1703c723cc1e92ca4bcbad34f26523714d8ca
anjalicg <chandrashekh@vmware.com>
2019-07-03 09:59:46 -0700

    methods of user input

--- src/github.com/anjalicg/04_user_input/main.go
+++ src/github.com/anjalicg/04_user_input/main.go
@@ -1,5 +1,7 @@
 package main
 
+//https://tutorialedge.net/golang/reading-console-input-golang/
+
 import (
 	"bufio"
 	"fmt"
@@ -13,31 +15,42 @@ func method1() {
 		fmt.Print("user>")
 		text, _ := reader.ReadString('\n')
 		fmt.Println("Hello", text)
+		fmt.Printf("type=%T\n", text)
 	}
 
 }
 func method2() {
+	// read one unicode from command line
 	reader := bufio.NewReader(os.Stdin)
 	char, _, err := reader.ReadRune()
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(char)
+	fmt.Printf("Type = %T\n", char)
 
 	switch char {
 	case 'A':
 		fmt.Println("A pressed")
 	case 'a':
 		fmt.Println("a pressed")
+	default:
+		fmt.Println("Neither A nor a pressed")
 	}
 
 }
 func method3() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		x := scanner.Text()
+		fmt.Println("User entered:-", x)
+		fmt.Printf("type=%T\n", x)
+	}
 
 }
 func main() {
-	method1()
-	// method2()
+	// method1()
+	method2()
 	// method3()
 
 }
